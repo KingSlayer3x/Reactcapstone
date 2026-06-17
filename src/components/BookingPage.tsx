fda5e1feb40b4d4d4006f872a1aad949934a4cd8
KingSlayer3x <alito321a@gmail.com>
2026-06-18 01:20:20 +0300

    update booking page

--- src/components/BookingPage.tsx
+++ src/components/BookingPage.tsx
@@ -1,7 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function BookingPage() {
+    interface BookingForm {
+        date: string;
+        time: string;
+        numberOfGuests: number | string;
+        occasion: string;
+    }
+    const [form, setForm] = useState<BookingForm>({
+        date: "",
+        time: "",
+        numberOfGuests: "",
+        occasion: "",
+    })
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement >) => {
+        setForm((prev) => ({
+            ...prev,
+            [e.target.name] : e.target.value
+        }));
+    };
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        const payload = {
+            ...form,
+            numberOfGuests: Number(form.numberOfGuests),
+        };
+    }
   return (
-    <div>BookingPage</div>
+    <div>
+        <form onSubmit={handleSubmit}>
+            <fieldset className='grid gap-2.5 w-max items-center mx-auto p-5'>
+                <legend className='text-center font-markazi text-5xl'>Book Your Table</legend>
+                    <label htmlFor="date">Date:</label>
+                        <input type="date" name='date' value={form.date} onChange={handleChange} />
+                    <label htmlFor="time" >Time</label>
+                        <input type="time" name='time' value={form.time} onChange={handleChange} />
+                    <label htmlFor='numberOfGuests'>Number Of Guests</label>
+                        <input type="number" name='numberOfGuests' value={form.numberOfGuests} onChange={handleChange} />
+                    <label htmlFor='occasion'>Occasion</label>
+                        <select name="occasion" id="occasion" value={form.occasion} onChange={handleChange}>
+                            <option value="">Select an occasion</option>
+                            <option value="birthday">Birthday</option>
+                            <option value="anniversary">Anniversary</option>
+                        </select>
+                    <button type='submit'>Book Now</button>
+            </fieldset>
+        </form>
+    </div>
   )
 }

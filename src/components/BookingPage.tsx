bb228b74e1e405100d70bc87d891b6bfed75a572
KingSlayer3x <alito321a@gmail.com>
2026-06-18 17:02:26 +0300

    minor booking page update

--- src/components/BookingPage.tsx
+++ src/components/BookingPage.tsx
@@ -7,12 +7,13 @@ export default function BookingPage() {
         numberOfGuests: number | string;
         occasion: string;
     }
-    const [form, setForm] = useState<BookingForm>({
+    const initialState = {
         date: "",
         time: "",
         numberOfGuests: "",
-        occasion: "",
-    })
+        occasion: ""
+    }
+    const [form, setForm] = useState(initialState)
     const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement >) => {
         setForm((prev) => ({
             ...prev,
@@ -21,6 +22,7 @@ export default function BookingPage() {
     };
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
+        setForm(initialState)
         const payload = {
             ...form,
             numberOfGuests: Number(form.numberOfGuests),
@@ -29,21 +31,21 @@ export default function BookingPage() {
   return (
     <div>
         <form onSubmit={handleSubmit}>
-            <fieldset className='grid gap-2.5 w-max items-center mx-auto p-5'>
-                <legend className='text-center font-markazi text-5xl'>Book Your Table</legend>
-                    <label htmlFor="date">Date:</label>
-                        <input type="date" name='date' value={form.date} onChange={handleChange} />
-                    <label htmlFor="time" >Time</label>
-                        <input type="time" name='time' value={form.time} onChange={handleChange} />
-                    <label htmlFor='numberOfGuests'>Number Of Guests</label>
-                        <input type="number" name='numberOfGuests' value={form.numberOfGuests} onChange={handleChange} />
-                    <label htmlFor='occasion'>Occasion</label>
-                        <select name="occasion" id="occasion" value={form.occasion} onChange={handleChange}>
-                            <option value="">Select an occasion</option>
-                            <option value="birthday">Birthday</option>
-                            <option value="anniversary">Anniversary</option>
+            <fieldset className='grid gap-2.5 md:w-max items-center mx-auto p-5 mt-7'>
+                <legend className='text-center font-markazi text-7xl'>Book Your Table</legend>
+                    <label htmlFor="date" className='font-karla text-4xl'>Date:</label>
+                        <input className='font-markazi border-2' type="date" name='date' id='date' value={form.date} onChange={handleChange} />
+                    <label htmlFor="time" className='font-markazi text-4xl'>Time</label>
+                        <input type="time" className='font-markazi border-2' name='time' id='time' value={form.time} onChange={handleChange} />
+                    <label htmlFor='numberOfGuests' className='text-4xl'>Number Of Guests</label>
+                        <input type="number" className='font-markazi border-2' id='numberOfGuests' name='numberOfGuests' value={form.numberOfGuests} onChange={handleChange} />
+                    <label htmlFor='occasion' className='font-markazi text-4xl'>Occasion</label>
+                        <select name="occasion" className='font-markazi text-2xl rounded-3xl' id="occasion" value={form.occasion} onChange={handleChange}>
+                            <option value="" className='bg-amber-100'>Select an occasion</option>
+                            <option value="birthday" className='bg-amber-200'>Birthday</option>
+                            <option value="anniversary" className='bg-amber-300'>Anniversary</option>
                         </select>
-                    <button type='submit'>Book Now</button>
+                    <button  type='submit' className='text-4xl rounded-2xl bg-amber-200 p-3 w-max text-center hover:bg-amber-300 hover:border-2'>Book Now</button>
             </fieldset>
         </form>
     </div>
